1b369b094b2e621d8bc529e4a89c0ef687796dc2
kumar2056 <42170484+kumar2056@users.noreply.github.com>
2018-08-07 14:17:25 +0530

    Create kl.java

---
+++ kl.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class kl
+{
+public static void main(String args[])
+{
+int a;
+Scanner j=new Scanner(System.in);
+a=j.nextInt();
+if(a==0)
+{
+System.out.println("entered number is zero");
+}
+else if(a<0)
+{
+System.out.println("entered number is negative");
+}
+else
+{
+System.out.println("entered number is positive");
+}
+}
+}
