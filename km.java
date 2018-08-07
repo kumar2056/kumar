97e7c8507c45cb0e4f71759d2ad5c8e5cc2a87c4
kumar2056 <42170484+kumar2056@users.noreply.github.com>
2018-08-07 14:23:01 +0530

    Create km.java

---
+++ km.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class km
+{
+public static void main(String args[])
+{
+int a;
+Scanner j=new Scanner(System.in);
+a=j.nextInt();
+if((a%2)==0)
+{
+System.out.println("entered number is even");
+}
+else if((a%2)!=0)
+{
+System.out.println("entered number is odd");
+}
+}
+}
